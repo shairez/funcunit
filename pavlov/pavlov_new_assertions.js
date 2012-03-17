e0288e796641947f73012e18e72865657b47f2d1
shai <shai@fashiontraffic.com>
2012-03-18 01:56:12 +0200

    added new pavlov assertions

--- pavlov/pavlov_new_assertions.js
+++ pavlov/pavlov_new_assertions.js
@@ -1,1 +1,16 @@
 /* new assertions */
+
+QUnit.specify.extendAssertions({
+    isGreaterThan: function(actual, expected, message) {
+        ok(actual > expected, message);
+    },
+    isLessThan: function(actual, expected, message) {
+        ok(actual < expected, message);
+    },
+    isNotEmpty: function(actual, message) {
+        ok(actual.length > 0, message);
+    },
+    sizeOf: function(actual, expected, message) {
+        ok(actual.length == expected, message);
+    }
+});
